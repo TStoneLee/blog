53874f97049ac6b718b46cbe4ff599ee0d753b3a
ThreeStonesLee <lileihdu@163.com>
2021-10-18 22:15:18 +0800

    更新配置&添加uniapp问题

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,16 +9,26 @@ module.exports = {
   // ],
   themeConfig: {
     logo: '/tongyang.jpeg',
+    lastUpdated: '更新于：', 
+    activeHeaderLinks: false, // 默认值：true
     nav: [
       { text: '首页', link: '/'},
-      { text: '进阶笔记', link: '/pages/advancedNotes/test.md'},
-      { text: '项目笔记', link: '/pages/developmentNotes/test.md'},
-      { text: '算法笔记', link: '/pages/algorithmNotes/test.md'},
-      { text: '读书笔记', link: '/pages/readNotes/test.md'},
+      { text: '进阶笔记', link: '/pages/advanced/test.md'},
+      { 
+        text: '项目笔记', 
+        ariaLabel: '项目笔记菜单',
+        // link: '/pages/development/test.md',
+        items: [
+          { text: 'uniapp开发', link: '/pages/development/uniapp/'},
+          { text: '其他项目',  link: '/pages/development/other/'}
+        ]
+      },
+      { text: '算法笔记', link: '/pages/algorithm/test.md'},
+      { text: '读书笔记', link: '/pages/read/test.md'},
       { text: 'GitHub', link: 'https://github.com/ThreeStonesLee'}
     ],
     sidebar: {
-      '/pages/advancedNotes/': [
+      '/pages/advanced/': [
         {
           title: 'JavaScript',
           collapsable: true,
@@ -45,11 +55,44 @@ module.exports = {
           ]
         }
       ],
-      '/pages/developmentNotes/': [
-        'mention.md'
+      '/pages/development/uniapp/': [
+        // '',
+        ['uni-icon.md', 'uniapp图标'],
+        // {
+        //   title: 'uniapp图标',
+        //   collapsable: true,
+        //   sidebarDepth: 1,
+        //   children: [
+        //     []
+        //   ]
+        // },
       ],
-      '/pages/algorithmNotes/': [],
-      '/pages/readNotes/': []
+      '/pages/development/other/': [
+        ['mention.md', '富文本@功能'],
+        ['vuepress.md', '搭建博客']
+        // {
+        //   title: 'uniapp图标',
+        //   collapsable: true,
+        //   sidebarDepth: 1,
+        //   children: [
+        //     []
+        //   ]
+        // },
+      ],
+      '/pages/development/': [
+        // {
+        //   title: '其他开发',
+        //   collapsable: true,
+        //   sidebarDepth: 1,
+        //   children: [
+        //     ['mention.md', '富文本@功能'],
+        //     ['vuepress.md', '搭建博客']
+        //   ]
+        // }
+        // 'mention.md'
+      ],
+      '/pages/algorithm/': [],
+      '/pages/read/': []
     }
   }
 }
---
+++ docs/pages/development/other/vuepress.md
@@ -0,0 +1,12 @@
+# 快速搭建博客并自动化部署GitHubPage Vuepress&Travis-ci&GitHub
+## 文档的目录结构
+  1. vuepress项目的文档结构必须是按照官方规范的格式进行书写，在固定的格式内，做特定的事情
+## 初始化项目
+  1. 创建并进入一个新目录 
+    ```javascript
+    mkdir vuepress-starter && cd vuepress-starter
+    ```
+  2. 
+## 基本配置
+## 部署流程 
+***第一次搭建，目前还在探索中，先不写文档，等到熟练了在一次完成*** 
